a9aa943a45d82e300c9b00966b19559de7785e59
TomLatin <tomlatin52@gmail.com>
2020-04-28 00:10:22 +0300

    done part a

--- Test.cpp
+++ Test.cpp
@@ -51,6 +51,17 @@ TEST_CASE("Test the function with RealVariable")
             CHECK(solve( (x^2) == 3136)==56);
             CHECK(solve( (x^2)  == 0)==0);
 
+            CHECK(solve( (x^2) == 1681)==41);
+            CHECK(solve( (x^2)  == 25921)==161);
+            CHECK(solve(( x^2)  == 361)==19);
+            CHECK(solve( (x^2) == 1000000)==1000);
+            CHECK(solve( (x^2)  == 8281)==91);
+            CHECK(solve( (x^2)  == 2704)==52);
+            CHECK(solve( (x^2) == 4096)==64);
+            CHECK(solve( (x^2)  == 12996)==114);
+            CHECK(solve( (x^2)  == 1234321)==1111);
+
+
             CHECK(( solve ( 6*(x^2)+6*x+4==4*(x^2)+2*x+4) == -2  ||  solve (6*(x^2)+6*x+4==4*(x^2)+2*x+4) == 0 ));
             CHECK(( solve ( 3*(x^2)+22*x+57==(x^2)-3) == -5  ||  solve (3*(x^2)+22*x+57==(x^2)-3) == -6 ));
             CHECK(( solve ( (-1*x^2)+6*x-4==-2*(x^2)-4) == -9  ||  solve ( (-1*x^2)+6*x-4==-2*(x^2)-4) == 0 ));
@@ -62,35 +73,62 @@ TEST_CASE("Test the function with RealVariable")
             CHECK(( solve ( 2*(x^2)-15*x+34==x+4) == 5  ||  solve (2*(x^2)-15*x+34==x+4) == 3 ));
             CHECK(( solve ( (x^2)-1*x-5==1 ) == -2  ||  solve ((x^2)-1*x-5==14) ==3 ));
 
-    CHECK_THROWS( solve (0*x==1)  );
-    CHECK_THROWS( solve (0*x==2346789)  );
-    CHECK_THROWS( solve (0*x==686868)  );
-    CHECK_THROWS( solve (0*x==9)  );
-    CHECK_THROWS( solve (0*x==166)  );
-    CHECK_THROWS( solve (0*x==15)  );
+            CHECK_THROWS( solve (0*x==1)  );
+            CHECK_THROWS( solve (0*x==2346789)  );
+            CHECK_THROWS( solve (0*x==686868)  );
+            CHECK_THROWS( solve (0*x==9)  );
+            CHECK_THROWS( solve (0*x==166)  );
+            CHECK_THROWS( solve (0*x==15)  );
+            CHECK_THROWS( solve (0*x==551)  );
+            CHECK_THROWS( solve (0*x==2346678789)  );
+            CHECK_THROWS( solve (0*x==68996868)  );
+            CHECK_THROWS( solve (0*x==900)  );
+            CHECK_THROWS( solve (0*x==169986)  );
+            CHECK_THROWS( solve (0*x==1777895)  );
 
-    CHECK_THROWS( solve (3*x-3*x==1)  );
-    CHECK_THROWS( solve (376*x-376*x==2346789)  );
-    CHECK_THROWS( solve (36*x-36*x==686868)  );
-    CHECK_THROWS( solve (8883*x-8883*x==9)  );
-    CHECK_THROWS( solve (37*x-37*x==166)  );
-    CHECK_THROWS( solve (9*x-9*x==15)  );
+            CHECK_THROWS( solve (0*x==11)  );
+            CHECK_THROWS( solve (0*x==23467189)  );
+            CHECK_THROWS( solve (0*x==6868168)  );
+            CHECK_THROWS( solve (0*x==91)  );
+            CHECK_THROWS( solve (0*x==1616)  );
+            CHECK_THROWS( solve (0*x==115)  );
+            CHECK_THROWS( solve (0*x==5151)  );
+            CHECK_THROWS( solve (0*x==23466178789)  );
+            CHECK_THROWS( solve (0*x==689961868)  );
+            CHECK_THROWS( solve (0*x==9100)  );
+            CHECK_THROWS( solve (0*x==1699816)  );
+            CHECK_THROWS( solve (0*x==17778915)  );
 
-    CHECK_THROWS( solve ((x^2)==-1)  );
-    CHECK_THROWS( solve ((x^2)==-2346789)  );
-    CHECK_THROWS( solve ((x^2)==-686868)  );
-    CHECK_THROWS( solve ((x^2)==-9)  );
-    CHECK_THROWS( solve ((x^2)==-166)  );
-    CHECK_THROWS( solve ((x^2)==-15)  );
+            CHECK_THROWS( solve (3*x-3*x==1)  );
+            CHECK_THROWS( solve (376*x-376*x==2346789)  );
+            CHECK_THROWS( solve (36*x-36*x==686868)  );
+            CHECK_THROWS( solve (8883*x-8883*x==9)  );
+            CHECK_THROWS( solve (37*x-37*x==166)  );
+            CHECK_THROWS( solve (9*x-9*x==15)  );
 
-    CHECK_THROWS( solve ((x^2)+4==(x^2)+4-166)  );
-    CHECK_THROWS( solve ((x^2)==(x^2)-15)  );
-    CHECK_THROWS( solve (8883*x==8883*x+9)  );
-    CHECK_THROWS( solve (37*x==37*x+166)  );
-    CHECK_THROWS( solve (9*x==9*x-15)  );
+            CHECK_THROWS( solve ((x^2)==-1)  );
+            CHECK_THROWS( solve ((x^2)==-2346789)  );
+            CHECK_THROWS( solve ((x^2)==-686868)  );
+            CHECK_THROWS( solve ((x^2)==-9)  );
+            CHECK_THROWS( solve ((x^2)==-166)  );
+            CHECK_THROWS( solve ((x^2)==-15)  );
+
+            CHECK_THROWS( solve ((x^2)+4==(x^2)+4-166)  );
+            CHECK_THROWS( solve ((x^2)==(x^2)-15)  );
+            CHECK_THROWS( solve (8883*x==8883*x+9)  );
+            CHECK_THROWS( solve (37*x==37*x+166)  );
+            CHECK_THROWS( solve (9*x==9*x-15)  );
+
+            CHECK_THROWS( solve ((5*x^2)+4==(5*x^2)+4-166)  );
+            CHECK_THROWS( solve ((55*x^2)==(55*x^2)-15)  );
+            CHECK_THROWS( solve (888553*x==888553*x+9)  );
+            CHECK_THROWS( solve (37*x==37*x+1655556)  );
+            CHECK_THROWS( solve (9*x==9*x-15555555555)  );
+
+
 }
 
-TEST_CASE("Test the function with RealVariable")
+TEST_CASE("Test the function with ComplexVariable")
 {
     ComplexVariable y;
             CHECK(( solve((y^2)==-4)==complex<double> (0,2) || solve((y^2)==-4)==complex<double> (0,-2) ));
